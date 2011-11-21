ec3339cdc8d9233a8738671fe0dcaa1f82ab57a0
pieero <pieero@localhost>
2011-11-21 06:06:05 +0100

    More features on logger

--- GraphicsLogger/GraphicsLogger.cpp
+++ GraphicsLogger/GraphicsLogger.cpp
@@ -2,16 +2,26 @@
 #include <QGraphicsItem>
 #include <QPainterPath>
 #include <QPainter>
+#include <QDir>
+#include <QTimer>
 
 GraphicsLogger::GraphicsLogger(QObject *parent) :
-    QGraphicsScene(parent)
+    QObject(parent)
+  , m_path(QDir::tempPath())
 {
+    m_timer.blockSignals(true);
+    m_timer.setSingleShot(true);
+    connect(&m_timer,SIGNAL(timeout()),this,SLOT(saveLog()));
 }
 
+void GraphicsLogger::setFilePath(QString p_path)
+{
+    m_path = p_path;
+}
 
 void GraphicsLogger::logMove(QPointF p_point,QString p_description)
 {
-    addLine(QLineF(m_position,p_point),QPen(QBrush(Qt::green),0.5,Qt::DotLine));
+    m_scene.addLine(QLineF(m_position,p_point),QPen(QBrush(Qt::green),0.5,Qt::DotLine));
     m_position = p_point;
     QRectF(m_position,p_point).center();
     logText(QRectF(m_position,p_point).center(),p_description);
@@ -19,13 +29,13 @@ void GraphicsLogger::logMove(QPointF p_point,QString p_description)
 
 void GraphicsLogger::logPoint(QPointF p_point,QString p_description)
 {
-    addEllipse(QRectF(m_position + p_point - QPointF(5,5),QSizeF(10,10)),QPen(Qt::NoPen),QBrush(Qt::blue));
+    m_scene.addEllipse(QRectF(m_position + p_point - QPointF(5,5),QSizeF(10,10)),QPen(Qt::NoPen),QBrush(Qt::blue));
     logText(m_position + p_point,p_description);
 }
 
 void GraphicsLogger::logLine(QPointF p_point,QString p_description)
 {
-    addLine(QLineF(m_position,p_point),QPen(QBrush(Qt::yellow),1));
+    m_scene.addLine(QLineF(m_position,p_point),QPen(QBrush(Qt::yellow),1));
     m_position = p_point;
     QRectF(m_position,p_point).center();
     logText(QRectF(m_position,p_point).center(),p_description);
@@ -38,7 +48,7 @@ void GraphicsLogger::logAngle(qreal from, qreal to,QString p_description)
     painter.begin(&anglePath);
     painter.drawPie(Text(p_point,QFont(),p_description);
     painter.end();
-    addPath(QLineF(m_position,p_point),QPen(QBrush(Qt::yellow),1));
+    m_scene.addPath(QLineF(m_position,p_point),QPen(QBrush(Qt::yellow),1));
     m_position = p_point;
     QRectF(m_position,p_point).center();
     logText(QRectF(m_position,p_point).center(),p_description);
@@ -51,4 +61,56 @@ void GraphicsLogger::logText(QPointF p_point,QString p_description)
     QGraphicsPathItem* text = addPath(textPath);
     text->setBrush(QBrush(Qt::blue));
     text->setPos(m_position + p_point);
+    logText(text->boundingRect().center(),p_description);
+}
+
+void GraphicsLogger::logItem(QGraphicsItem* p_item, QString p_description)
+{
+    QGraphicsPathItem* text = addPath(p_item->mapToScene(p_item->shape()));
+    text->setBrush(QBrush(Qt::blue));
+    text->setPos(m_position + p_point);
+    logText(text->boundingRect().center(),p_description);
+}
+
+void GraphicsLogger::logPath(QPainterPath* p_item, QString p_description)
+{
+    QGraphicsPathItem* text = addPath(textPath);
+    text->setBrush(QBrush(Qt::blue));
+    text->setPos(m_position + p_point);
+    logText(text->boundingRect().center(),p_description);
+}
+
+void GraphicsLogger::clearLog()
+{
+    m_scene.clear();
+    m_counter = 1;
+}
+
+void GraphicsLogger::requestSave()
+{
+    m_timer.blockSignals(true);
+    m_timer.stop();
+    m_timer.start(500);
+    m_timer.blockSignals(false);
+}
+
+void GraphicsLogger::saveLog()
+{
+//![configure SVG generator]
+    QSvgGenerator generator;
+    generator.setFileName(m_path);
+    generator.setSize(QSize(200, 200));
+    generator.setViewBox(QRect(0, 0, 200, 200));
+    generator.setTitle(tr("SVG Generator Example Drawing"));
+    generator.setDescription(tr("An SVG drawing created by the SVG Generator "
+                                "Example provided with Qt."));
+//![configure SVG generator]
+//![begin painting]
+    QPainter painter;
+    painter.begin(&generator);
+//![begin painting]
+    paint(&painter,ui->graphicsView->scene());
+//![end painting]
+    painter.end();
+//![end painting]
 }
--- GraphicsLogger/GraphicsLogger.h
+++ GraphicsLogger/GraphicsLogger.h
@@ -3,27 +3,37 @@
 
 #include <QGraphicsScene>
 
-class GraphicsLogger : public QGraphicsScene
+class GraphicsLogger : public QObject
 {
     Q_OBJECT
 public:
     explicit GraphicsLogger(QObject *parent = 0);
 
     void setFilePath(QString p_path);
-    void requestSave();
 
     void logMove(QPointF p_point,QString p_description);
     void logPoint(QPointF p_point,QString p_description);
     void logLine(QPointF p_point,QString p_description);
     void logAngle(qreal from, qreal to,QString p_description);
+    void logItem(QGraphicsItem* p_item, QString p_description);
+    void logPath(QPainterPath* p_item, QString p_description);
+    void clearLog();
+
+protected:
+    void requestSave();
     void logText(QPointF p_point,QString p_description);
 
 signals:
 
-public slots:
+private slots:
+    void saveLog();
 
 private:
     QPointF m_position;
+    QGraphicsScene m_scene;
+    unsigned int m_counter;
+    QString m_path;
+    QTimer m_timer;
 
 };
 
